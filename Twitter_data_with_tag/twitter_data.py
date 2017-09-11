9ea21bf4f89037f1799afb71596b2db0fe6d03ed
vermanil <anilrajverma1996@gmail.com>
2017-09-11 11:38:50 +0530

    gernalize code

--- Twitter_data_with_tag/twitter_data.py
+++ Twitter_data_with_tag/twitter_data.py
@@ -1,15 +1,17 @@
 import tweepy
-consumer_key= 'QVudSTJo7kvR0UGrlI5FmOQz8'
-consumer_secret= 'CHLcbmM9k6eWKNddX7OOsY6ChaeMP4rtGbTuNYmKGcOGanVWSh'
-access_token='865442933842956289-Bwt1ZPyUmMKh5QotQwTzLFCRmuMRf9i'
-access_token_secret='lXSQOzs5DtjakBNgFpDrliDVxIUBFWUHJjCcPyQlkIOos'
+consumer_key= 'Add your consumer Key'
+consumer_secret= 'Add consumer Secret Key'
+access_token='Add access token'
+access_token_secret='add access secret token'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
-arr = ['oyo room','good']
+sentence = raw_input("Enter your tag u want to search")
+arr = sentence.split(" ")
+# arr = ['oyo room','good']
 
 public_tweets = api.search(arr)
 # print public_tweets.text
